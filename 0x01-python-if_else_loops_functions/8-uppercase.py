34c4135d7184135da8cb9cf886f257c65ae787f7
osazee25 <osazeemarvins@yahoo.com>
2023-09-05 17:58:21 -0700

    python

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-def upper_conv(char):
-    if ord(char) >= 97 and ord(char) <= 122:
-        return (ord(char) - 32)
+def to_uper(character):
+    if ord(character) >= 97 and ord(character) <= 122:
+        return (ord(character) - 32)
     else:
-        return ord(char)
+        return ord(character)
+
 
 def uppercase(string):
-    string_upper = ""
-    for char in string:
-        string_upper += "%c" % upper_conv(char)
-    print("{:s}".format(string_upper))
+    string_new = ""
+    for character in string:
+        string_new += "%c" % to_uper(character)
+    print("{:s}".format(string_new))
