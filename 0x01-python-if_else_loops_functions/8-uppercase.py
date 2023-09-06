5ea900fef923e7d46ea0c45086c53a843e74e43e
osazee25 <osazeemarvins@yahoo.com>
2023-09-05 17:54:37 -0700

    python

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,12 +2,12 @@
 
 def upper_conv(char):
     if ord(char) >= 97 and ord(char) <= 122:
-        return chr(ord(char) - 32)  # Convert to uppercase and return the character
+        return (ord(char) - 32)
     else:
-        return char  # If not a lowercase letter, return the character as is
+        return ord(char)
 
 def uppercase(string):
     string_upper = ""
     for char in string:
-        string_upper += upper_conv(char)
+        string_upper += "%c" % upper_conv(char)
     print("{:s}".format(string_upper))
