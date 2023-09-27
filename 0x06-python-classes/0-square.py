0116699d59d94ddb089ddfd55c323465a3e38019
osazee25 <osazeemarvins@yahoo.com>
2023-09-26 20:22:48 -0700

    classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines a class square"""
+
+
+class Square:
+    """A class of object that squares"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A class definition for square"""
+
+
+class Square:
+    """Instantiation of the square with size base on 0-square.py
+       Attributes:
+            size: size of square
+        """
+    def __init__(self, size):
+        """creates a new instance of the square
+           Args:
+                size:one sided
+
+            """
+
+        self.__size = size
