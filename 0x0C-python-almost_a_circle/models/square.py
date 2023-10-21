973a4d521cca546f3dcf199f74dd309a07060d5c
osazee25 <osazeemarvins@yahoo.com>
2023-10-20 18:33:21 -0700

    circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -31,31 +31,37 @@ class Square(Rectangle):
         self.__height = value
 
     def update(self, *args, **kwargs):
-        """Updates attributes of an instance"""
+    """
+    Update the attributes of the Square.
 
-        if args is not None and len(args) != 0:
-            if len(args) >= 1:
-                if type(args[0]) != int and args[0] is not None:
-                    raise TypeError("id must be an integer")
-                self.id = args[0]
-            if len(args) > 1:
-                self.size = args[1]
-            if len(args) > 2:
-                self.x = args[2]
-            if len(args) > 3:
-                self.y = args[3]
-        else:
-            for key, value in kwargs.items():
-                if key == "id":
-                    if type(value) != int and value is not None:
+    This method allows you to update the attributes of the Square
+    using either positional arguments (*args) or keyword arguments (**kwargs).
+
+    Args:
+        *args: Positional arguments in the order (id, size, x, y).
+        **kwargs: Keyword arguments with attribute names as keys.
+
+    Raises:
+        TypeError: If 'id' is not an integer (if provided).
+
+    Returns:
+        None
+    """
+    if args:
+        attributes = ["id", "size", "x", "y"]
+        for i, arg in enumerate(args):
+            if arg is not None:
+                if i == 0:
+                    if not isinstance(arg, int):
                         raise TypeError("id must be an integer")
-                    self.id = value
-                if key == "size":
-                    self.size = value
-                if key == "x":
-                    self.x = value
-                if key == "y":
-                    self.y = value
+                setattr(self, attributes[i], arg)
+    
+    if kwargs:
+        for key, value in kwargs.items():
+            if key in ("id", "size", "x", "y") and value is not None:
+                if key == "id" and not isinstance(value, int):
+                    raise TypeError("id must be an integer")
+                setattr(self, key, value)
 
     def to_dictionary(self):
         """Returns the dictionary representation of a Square"""
