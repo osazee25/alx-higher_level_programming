d0079d1f9260c9aaa36b9f907e941ac7b6740f50
osazee25 <osazeemarvins@yahoo.com>
2023-12-12 18:24:41 -0800

    oojs

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.nbOccurrences = function (list, searchElement) {
+  let count = 0;
+
+  for (let i = 0; i < list.length; i++) {
+    count += list[i] === searchElement ? 1 : 0;
+  }
+
+  return count;
+};
